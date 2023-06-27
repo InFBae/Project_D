148823da3ee3793ee7105b375a348762b775b7a0
JUNG BOBAE <acarid27@gmail.com>
2023-06-28 00:42:44 +0900

    feat: Add SceneLoad
    주방, 주방 통로, 실험실 통로 추가
    도서관 수정

---
+++ Assets/Scripts/Scenes/CorridorKitchenScene.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorridorKitchenScene : BaseScene
+{
+    [SerializeField] List<Transform> spawnPoints;
+    [SerializeField] GameObject player;
+    protected override IEnumerator LoadingRoutine(string exScene)
+    {
+        // 플레이어 위치 이동
+        if (exScene == "GameTitleScene")
+        {
+            if (GameManager.Data.PlayerStatusData.savedSpawnPoint == null)
+            {
+                GameManager.Data.PlayerTransform = spawnPoints[0];
+            }
+            else
+            {
+                GameManager.Data.PlayerTransform = GameManager.Data.PlayerStatusData.savedSpawnPoint;
+            }
+        }
+        else if (exScene == "Scenes/DungeonMaps/Room_Library")
+        {
+            GameManager.Data.PlayerTransform = spawnPoints[0];
+        }
+        else if (exScene == "Scenes/DungeonMaps/Room_Kitchen")
+        {
+            GameManager.Data.PlayerTransform = spawnPoints[1];
+        }
+
+        player.SetActive(false);
+
+        player.transform.position = GameManager.Data.PlayerTransform.position;
+
+        player.SetActive(true);
+
+
+        progress = 0.5f;
+        yield return null;
+        // 씬의 몬스터 리스폰
+
+        progress = 1f;
+
+    }
+
+    private void OnDestroy()
+    {
+        // TODO 모든 몬스터 삭제
+    }
+}
